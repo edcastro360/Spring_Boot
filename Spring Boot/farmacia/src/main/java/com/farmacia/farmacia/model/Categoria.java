90a2a423cf0b379cf8faf0a4c45cec19534d01c1
edcastro360 <76817966+edcastro360@users.noreply.github.com>
2021-05-03 21:53:14 -0300

    Add files via upload

---
+++ Spring Boot/farmacia/src/main/java/com/farmacia/farmacia/model/Categoria.java
@@ -0,0 +1,65 @@
+package com.farmacia.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "produtos")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 1, max = 50)
+	private String marca;
+	
+	@NotNull
+	@Size(min = 5, max = 500)
+	private String descricao;
+	
+	@NotNull
+	@Size(min = 1, max = 20)
+	private String segmento;
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String nome) {
+		this.marca = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getSegmento() {
+		return segmento;
+	}
+
+	public void setSegmento(String titulo) {
+		this.segmento = titulo;
+	}
+
+	
+}
